ccaa16146e3ec4d3304a04ae4a913a088697e0a9
TempusSkyless <84135433+TempusSkyless@users.noreply.github.com>
2021-05-13 11:16:54 -0400

    nuevo comando

--- src/restaurant/Restaurant.java
+++ src/restaurant/Restaurant.java
@@ -15,9 +15,7 @@ public class Restaurant {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        //hola
-        //estoy modificando
+        System.out.println("impriame esta ");
     }
     
 }
